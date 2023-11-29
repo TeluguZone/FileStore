da3594405f89083165c8711524b4cb9f1b2076e2
TeluguZone <140744388+TeluguZone@users.noreply.github.com>
2023-11-29 08:17:20 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -4,15 +4,15 @@ import os
 
 
 class Config(object):
-	API_ID = int(os.environ.get("API_ID", "0"))
-	API_HASH = os.environ.get("API_HASH")
-	BOT_TOKEN = os.environ.get("BOT_TOKEN")
-	BOT_USERNAME = os.environ.get("BOT_USERNAME")
-	DB_CHANNEL = int(os.environ.get("DB_CHANNEL", "-100"))
-	BOT_OWNER = int(os.environ.get("BOT_OWNER", "1445283714"))
-	DATABASE_URL = os.environ.get("DATABASE_URL")
+	API_ID = int(os.environ.get("API_ID", "24830912"))
+	API_HASH = os.environ.get("API_HASH","a1a1775593531b90850b8b82e3b14940")
+	BOT_TOKEN = os.environ.get("BOT_TOKEN","6757671040:AAFO49zI8x3mT6glKU_8OKo4aPuoWlmw4uA")
+	BOT_USERNAME = os.environ.get("DaemonsXbot")
+	DB_CHANNEL = int(os.environ.get("DB_CHANNEL", "-1002127374415"))
+	BOT_OWNER = int(os.environ.get("BOT_OWNER", "809614790"))
+	DATABASE_URL = os.environ.get("mongodb+srv://Vasu:Teluguzone@cluster0.cf3e2zg.mongodb.net/?retryWrites=true&w=majority")
 	UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "")
-	LOG_CHANNEL = os.environ.get("LOG_CHANNEL", None)
+	LOG_CHANNEL = os.environ.get("-1001611267555", None)
 	BANNED_USERS = set(int(x) for x in os.environ.get("BANNED_USERS", "1234567890").split())
 	FORWARD_AS_COPY = bool(os.environ.get("FORWARD_AS_COPY", True))
 	BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", False))
